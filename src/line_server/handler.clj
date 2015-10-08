e099af62e79e7882b8d01c38c2c6d35c7042e143
Rebecca Paz <r.pazyaquian@gmail.com>
2015-10-08 13:12:17 -0400

    add handlers for files, line numbers

--- src/line_server/handler.clj
+++ src/line_server/handler.clj
@@ -1,9 +1,26 @@
 (ns line-server.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [line-server.file :as f]))
+
+(defn handle-get-line [files-lines file-name line-number]
+  (let [file-line (f/get-line files-lines file-name line-number)]
+    (if (= file-line :error)
+      {:status 413}
+      {:status 200
+       :headers {"Content-Type" "text/html; charset=utf-8"}
+       :body file-line})))
+
+(def file-names-and-paths {"50" "resources/public/50.txt"
+                           "5000" "resources/public/5000.txt"
+                           "50000" "resources/public/50000.txt"})
+
+(def files-lines (f/make-files-lines file-names-and-paths))
 
 (defroutes app-routes
+  (GET "/:file-name/lines/:line-number" [file-name line-number]
+       (handle-get-line files-lines file-name line-number))
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
